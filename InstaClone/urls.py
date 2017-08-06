7d86c179367b78c62508f1721d5939da786528d6
Saksham Mahajan <mahajansaksham38@gmail.com>
2017-08-06 14:25:13 +0530

    Update urls.py

--- InstaClone/urls.py
+++ InstaClone/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from myapp.views import signup_view,login_view,feed_view,post_view,like_view,comment_view
+from myapp.views import signup_view,login_view,feed_view,post_view,like_view,comment_view,logout_view,upvote_view
 
 urlpatterns = [
   url(r'^admin/', admin.site.urls),
   url('logout',logout_view),
+  url('upvote/', upvote_view),
   url('post', post_view),
   url('feed', feed_view),
   url('like', like_view),
